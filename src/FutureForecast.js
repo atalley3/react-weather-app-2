74887960bdd98615174bf8869f3550a3fae91c79
atalley3 <ashley.l.talley@gmail.com>
2022-05-11 22:35:04 -0400

    update FutureForecast.js
    updated rendering
    also confimred the while loop works

--- src/FutureForecast.js
+++ src/FutureForecast.js
@@ -5,19 +5,6 @@ import "./FutureForecast.css";
 export default function FutureForecast(props) {
   let forecast = props.forecast;
 
-  if (forecast.length === 1) {
-    return (
-      <div className="FutureForecast">
-        <DailyForecast forecast={forecast[0]} day="Mon" />
-        <DailyForecast forecast={forecast[0]} day="Tues" />
-        <DailyForecast forecast={forecast[0]} day="Wed" />
-        <DailyForecast forecast={forecast[0]} day="Thur" />
-        <DailyForecast forecast={forecast[0]} day="Fri" />
-        <DailyForecast forecast={forecast[0]} day="Sat" />
-        <DailyForecast forecast={forecast[0]} day="Sun" />
-      </div>
-    );
-  }
   if (forecast.length === 8) {
     return (
       <div className="FutureForecast">
@@ -28,6 +15,18 @@ export default function FutureForecast(props) {
         <DailyForecast forecast={forecast[5]} day={null} />
         <DailyForecast forecast={forecast[6]} day={null} />
         <DailyForecast forecast={forecast[7]} day={null} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="FutureForecast">
+        <DailyForecast forecast={forecast[0]} day="Mon" />
+        <DailyForecast forecast={forecast[0]} day="Tues" />
+        <DailyForecast forecast={forecast[0]} day="Wed" />
+        <DailyForecast forecast={forecast[0]} day="Thur" />
+        <DailyForecast forecast={forecast[0]} day="Fri" />
+        <DailyForecast forecast={forecast[0]} day="Sat" />
+        <DailyForecast forecast={forecast[0]} day="Sun" />
       </div>
     );
   }
